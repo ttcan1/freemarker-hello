a1ccdf9f533e2b9f9e92d4f8a984854182e80537
Can Tran <cantran@mijoconnected.com>
2024-07-17 09:09:52 +0700

    Add gradle

--- build/scripts/myproject.bat
+++ build/scripts/myproject.bat
@@ -66,7 +66,7 @@ set CMD_LINE_ARGS=%*
 set CLASSPATH=%APP_HOME%\lib\myproject.jar;%APP_HOME%\lib\guava-23.0.jar;%APP_HOME%\lib\freemarker-2.3.31.jar;%APP_HOME%\lib\jsr305-1.3.9.jar;%APP_HOME%\lib\error_prone_annotations-2.0.18.jar;%APP_HOME%\lib\j2objc-annotations-1.1.jar;%APP_HOME%\lib\animal-sniffer-annotations-1.14.jar
 
 @rem Execute myproject
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %MYPROJECT_OPTS%  -classpath "%CLASSPATH%" com.example.GenerateCppFromTemplate %CMD_LINE_ARGS%
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %MYPROJECT_OPTS%  -classpath "%CLASSPATH%" com.example.GenerateIdlFromTemplate %CMD_LINE_ARGS%
 
 :end
 @rem End local scope for the variables with windows NT shell
